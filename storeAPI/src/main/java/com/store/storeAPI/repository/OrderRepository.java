8aad620aaceebe30dec4f27ed5d8209f9f2d7ab7
Adam Mokhtar <adammokhtar@Adams-MacBook-Pro.local>
2023-01-24 16:36:19 +0100

    added the connection to the db via docker postgres image

--- storeAPI/src/main/java/com/store/storeAPI/repository/OrderRepository.java
+++ storeAPI/src/main/java/com/store/storeAPI/repository/OrderRepository.java
@@ -5,6 +5,10 @@ import com.store.storeAPI.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.List;
+
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
+    //another way of doing find all
+    //List<Order> findAll();
 }
